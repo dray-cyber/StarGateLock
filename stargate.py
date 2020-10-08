0cf410ef43648d9c4b957bf52dcee0b68e03fa4e
dray-cyber <66476272+dray-cyber@users.noreply.github.com>
2020-10-07 17:41:14 -0700

    stargate.py duh

---
+++ stargate.py
@@ -0,0 +1,621 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import Tk, Label, Button
+import json
+from pathlib import Path
+root = Tk()
+apple = 3
+if apple != 1:
+    print("program has reset and works")
+    symb1 = 0
+    symb2 = 0
+    symb3 = 0
+    symb4 = 0
+    symb5 = 0
+    symb6 = 0
+    symb7 = 0
+    symb8 = 0
+    symb9 = 0
+    symb10 = 0
+
+    symb11 = 0
+    symb12 = 0
+    symb13 = 0
+    symb14 = 0
+    symb15 = 0
+    symb16 = 0
+    symb17 = 0
+    symb18 = 0
+    symb19 = 0
+    symb20 = 0
+
+    symb21 = 0
+    symb22 = 0
+    symb23 = 0
+    symb24 = 0
+    symb25 = 0
+    symb26 = 0
+    symb27 = 0
+    symb28 = 0
+    symb29 = 0
+    symb30 = 0
+    arraystatus = 0
+    symb31 = 0
+    symb32 = 0
+    symb33 = 0
+    symb34 = 0
+    symb35 = 0
+    symb36 = 0
+    symb37 = 0
+    symb38 = 0
+    apple = 1
+    print("im not still running right")
+arraystatuse = arraystatus
+print(arraystatus)
+global sequence
+sequence = []
+def sym1():
+    global symb1
+    symb1 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        print(arraystatus)
+        sequence.append("sym1")
+        print(sequence)
+def sym2(sy1=symb1):
+    global symb2
+    symb2 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym2")
+def sym3():
+    global symb3
+    symb3 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym3")
+def sym4():
+    global symb4
+    symb4 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym4")
+def sym5():
+    global symb5
+    symb5 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym5")
+def sym6():
+    global symb6
+    symb6 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym6")
+def sym7():
+    global symb7
+    symb7 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym7")
+def sym8():
+    global symb8
+    symb8 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym8")
+def sym9():
+    global symb9
+    symb9 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym9")
+def sym10():
+    global symb10
+    symb10 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym10")
+def sym11():
+    global symb11
+    symb11 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym11")
+def sym12():
+    global symb12
+    symb12 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym12")
+def sym13():
+    global symb13
+    symb13 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym13")
+def sym14():
+    global symb14
+    symb14 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym14")
+def sym15():
+    global symb15
+    symb15 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym15")
+def sym16():
+    global symb16
+    symb16 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym16")
+def sym17():
+    global symb17
+    symb17 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym17")
+def sym18():
+    global symb18
+    symb18 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym18")
+def sym19():
+    global symb19
+    symb19 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym19")
+
+def sym20():
+    global symb20
+    symb20 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym20")
+def sym21():
+    global symb21
+    symb21 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym21")
+def sym22():
+    global symb22
+    symb22 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym22")
+def sym23():
+    global symb23
+    symb23 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym23")
+def sym24():
+    global symb24
+    symb24 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym24")
+def sym25():
+    global symb25
+    symb25 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym25")
+def sym26():
+    global symb26
+    symb26 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym26")
+def sym27():
+    global symb27
+    symb27 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym27")
+def sym28():
+    global symb28
+    symb28 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym28")
+def sym29():
+    global symb29
+    symb29 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym29")
+
+
+def sym30():
+    global symb30
+    symb30 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym30")
+
+
+def sym31():
+    global symb31
+    symb31 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym31")
+
+
+def sym32():
+    global symb32
+    symb32 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym32")
+
+
+def sym33():
+    global symb33
+    symb33 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym33")
+
+
+def sym34():
+    global symb34
+    symb34 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym34")
+
+
+def sym35():
+    global symb35
+    symb35 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym35")
+
+
+def sym36():
+    global symb36
+    symb36 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym36")
+
+
+def sym37():
+    global symb37
+    symb37 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym37")
+
+
+def sym38():
+    global symb38
+    symb38 = 1
+    global arraystatus
+    arraystatus = arraystatus + 1
+    global sequence
+    if arraystatus <10:
+        sequence.append("sym38")
+
+def symdome():
+    global sequence
+    stargatecode = Path("data.json")
+    sequencestr = str(sequence)
+    datat = sequencestr.replace(", ", "")
+    datatt = datat.replace("\'", " ")
+    datattt = datatt.replace("[", "")
+    datatttt = datattt.replace("   ", "  ")
+    datattttt = datatttt.replace("]", "")
+    finaldata = datattttt.replace("\"", "")
+    datacompare = finaldata.replace("  ", " ")
+    if stargatecode.is_file():
+        f = open('data.json', )
+        data = json.load(f)
+        comparestring = str(data)
+        print(comparestring)
+        print(datacompare)
+        if datacompare == comparestring:
+            print("all your code works you may login")
+        f.close
+    else:
+        print("creating new file")
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
+
+
+Label(root, text='StarGateLock', font=(
+    'Verdana', 15)).pack(side=TOP, pady=10)
+
+
+photo1 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g1.png")
+photo2 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g2.png")
+photo3 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g3.png")
+photo4 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g4.png")
+photo5 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g5.png")
+photo6 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g6.png")
+photo7 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g7.png")
+photo8 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g8.png")
+photo9 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g9.png")
+photo10 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g10.png")
+
+photo11 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g11.png")
+photo12 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g12.png")
+photo13 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g13.png")
+photo14 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g14.png")
+photo15 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g15.png")
+photo16 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g16.png")
+photo17 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g17.png")
+photo18 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g18.png")
+photo19 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g19.png")
+photo20 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g20.png")
+
+photo21 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g21.png")
+photo22 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g22.png")
+photo23 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g23.png")
+photo24 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g24.png")
+photo25 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g25.png")
+photo26 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g26.png")
+photo27 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g27.png")
+photo28 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g28.png")
+photo29 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g29.png")
+photo30 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g30.png")
+
+photo31 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g31.png")
+photo32 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g32.png")
+photo33 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g33.png")
+photo34 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g34.png")
+photo35 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g35.png")
+photo36 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g36.png")
+photo37 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g37.png")
+photo38 = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\g38.png")
+photodome = PhotoImage(file=r"C:\Users\Dray\Desktop\stargatelock\dhd.png")
+
+photoimage1 = photo1.subsample(3, 3)
+photoimage2 = photo2.subsample(3, 3)
+photoimage3 = photo3.subsample(3, 3)
+photoimage4 = photo4.subsample(3, 3)
+photoimage5 = photo5.subsample(3, 3)
+photoimage6 = photo6.subsample(3, 3)
+photoimage7 = photo7.subsample(3, 3)
+photoimage8 = photo8.subsample(3, 3)
+photoimage9 = photo9.subsample(3, 3)
+photoimage10 = photo10.subsample(3, 3)
+
+photoimage11 = photo11.subsample(3, 3)
+photoimage12 = photo12.subsample(3, 3)
+photoimage13 = photo13.subsample(3, 3)
+photoimage14 = photo14.subsample(3, 3)
+photoimage15 = photo15.subsample(3, 3)
+photoimage16 = photo16.subsample(3, 3)
+photoimage17 = photo17.subsample(3, 3)
+photoimage18 = photo18.subsample(3, 3)
+photoimage19 = photo19.subsample(3, 3)
+photoimage20 = photo20.subsample(3, 3)
+
+photoimage21 = photo21.subsample(3, 3)
+photoimage22 = photo22.subsample(3, 3)
+photoimage23 = photo23.subsample(3, 3)
+photoimage24 = photo24.subsample(3, 3)
+photoimage25 = photo25.subsample(3, 3)
+photoimage26 = photo26.subsample(3, 3)
+photoimage27 = photo27.subsample(3, 3)
+photoimage28 = photo28.subsample(3, 3)
+photoimage29 = photo29.subsample(3, 3)
+photoimage30 = photo30.subsample(3, 3)
+
+photoimage31 = photo31.subsample(3, 3)
+photoimage32 = photo32.subsample(3, 3)
+photoimage33 = photo33.subsample(3, 3)
+photoimage34 = photo34.subsample(3, 3)
+photoimage35 = photo35.subsample(3, 3)
+photoimage36 = photo36.subsample(3, 3)
+photoimage37 = photo37.subsample(3, 3)
+photoimage38 = photo38.subsample(3, 3)
+photoimagedome = photodome.subsample(3, 3)
+
+Button(root, image=photoimage1, # 1
+       command=sym1).place(x=20,y=10)
+
+Button(root, image=photoimage2, # 2
+       command=sym2).place(x=60,y=10)
+
+Button(root, image=photoimage3, # 3
+       command=sym3).place(x=100,y=10)
+
+Button(root, image=photoimage4, # 4
+       command=sym4).place(x=140,y=10)
+
+Button(root, image=photoimage5, # 5
+       command=sym5).place(x=180,y=10)
+
+Button(root, image=photoimage6, # 6
+       command=sym6).place(x=220,y=10)
+
+Button(root, image=photoimage7, # 7
+       command=sym7).place(x=260,y=10)
+
+Button(root, image=photoimage8, # 8
+       command=sym8).place(x=300,y=10)
+
+Button(root, image=photoimage9, # 9
+       command=sym9).place(x=340,y=10)
+
+Button(root, image=photoimage10, #10
+       command=sym10).place(x=380,y=10)
+
+Button(root, image=photoimage11, #11
+       command=sym11).place(x=420,y=10)
+
+Button(root, image=photoimage12, #12
+       command=sym12).place(x=460,y=10)
+
+Button(root, image=photoimage13,#13
+       command=sym13).place(x=500,y=10)
+
+Button(root, image=photoimage14,#14
+       command=sym14).place(x=540,y=10)
+
+Button(root, image=photoimage15,#15
+       command=sym15).place(x=580,y=10)
+
+Button(root, image=photoimage16,#16
+       command=sym16).place(x=620,y=10)
+
+Button(root, image=photoimage17,#17
+       command=sym17).place(x=660,y=10)
+
+Button(root, image=photoimage18,#18
+       command=sym18).place(x=700,y=10)
+
+Button(root, image=photoimage19,#19
+       command=sym19).place(x=740,y=10)
+
+Button(root, image=photoimage20,#20
+       command=sym20).place(x=780,y=10)
+
+Button(root, image=photoimage21,#21
+       command=sym21).place(x=820,y=10)
+
+Button(root, image=photoimage22,#22
+       command=sym22).place(x=860,y=10)
+
+Button(root, image=photoimage23,#23
+       command=sym23).place(x=900,y=10)
+
+Button(root, image=photoimage24,#24
+       command=sym24).place(x=940,y=10)
+
+Button(root, image=photoimage25,#25
+       command=sym25).place(x=980,y=10)
+
+Button(root, image=photoimage26,#26
+       command=sym26).place(x=1020,y=10)
+
+Button(root, image=photoimage27,#27
+       command=sym27).place(x=1060,y=10)
+
+Button(root, image=photoimage28,#28
+       command=sym28).place(x=1100,y=10)
+
+Button(root, image=photoimage29,#29
+       command=sym29).place(x=1140,y=10)
+
+Button(root, image=photoimage30,#30
+       command=sym30).place(x=1180,y=10)
+
+Button(root, image=photoimage31,#31
+       command=sym31).place(x=1220,y=10)
+
+Button(root, image=photoimage32,#32
+       command=sym32).place(x=20,y=50)
+
+Button(root, image=photoimage33,#33
+       command=sym33).place(x=60,y=50)
+
+Button(root, image=photoimage34,#34
+       command=sym34).place(x=100,y=50)
+
+Button(root, image=photoimage35,#35
+       command=sym35).place(x=140,y=50)
+
+Button(root, image=photoimage36,#36
+       command=sym36).place(x=180,y=50)
+
+Button(root, image=photoimage37,#37
+       command=sym37).place(x=220,y=50)
+
+Button(root, image=photoimage38,#38
+       command=sym38).place(x=260,y=50)
+
+Button(root, image=photoimagedome,#dome
+       command=symdome).place(x=1100,y=550)
+
+stargatecode = Path("data.json")
+if stargatecode.is_file():
+    print("theres a file :c")
+else:
+    print("nope no file")
+mainloop()
