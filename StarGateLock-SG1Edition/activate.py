ba02457a8cb58bc63782abea1136f95569d5d682
dray-cyber <66476272+dray-cyber@users.noreply.github.com>
2020-10-16 15:22:49 -0700

    Revamp

---
+++ StarGateLock-SG1Edition/activate.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+import os
+root = tk.Tk()
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+if worked == False:
+    if screen_height == 720 and screen_width == 1280:
+        os.system('cmd /c pythonw.exe 1280x720.py')
+        worked = True
+    if screen_height == 1080 and screen_width == 1920:
+        os.system('cmd /c pythonw.exe 1920x1080.py')
+        worked = True
+    if screen_height == 768 and screen_width == 1366:
+        os.system('cmd /c pythonw.exe 1366x768.py')
+        worked = True
+    if screen_height == 768 and screen_width == 1280:
+        os.system('cmd /c pythonw.exe 1280x768.py')
+        worked = True
+else:
+    os.system('cmd /c pythonw.exe stargate.py')
